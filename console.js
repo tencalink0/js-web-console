95d412d3af0e95b5a1f5a6bf8b4efc69f39946f7
bev29rr <ben.varea@gmail.com>
2024-11-05 23:15:35 +0000

    development of debug tool

--- console.js
+++ console.js
@@ -1,6 +1,15 @@
+let variables = [];
+
 function runCode() {
     const code = document.getElementById('codeInput').value.replaceAll('console.log', 'window.alert');
     const outputDiv = document.getElementById('output');
+
+    let variableCheck = false;
+    while (variableCheck != true) {
+        getVars('let');
+        variableCheck = true;
+    }
+
     try {
         const wrappedCode = `(function() { ${code} })()`;
         const result = eval(wrappedCode);
@@ -9,5 +18,13 @@ function runCode() {
         }
     } catch (error) {
         window.alert('Error: ' + error.message);
+    }
+}
+
+function getVars(type) {
+    let lastVar = 0;
+    let pos = code.indexOf("let", lastVar);
+    if (pos != -1) {
+        window.alert(code[pos + type.length]);
     }
 }
