6a5b71614fcbcd8333d6fb41d32609021e213fad
Ben <ben.varea@gmail.com>
2024-11-09 00:24:29 +0000

    bug-fixes and buidl guide written up

--- console.js
+++ console.js
@@ -1,20 +1,22 @@
-let variables = [[],[],[]];
+let variables = [[],[],[],[]];
 let state = true; 
 let focusOnTerm = 0;
-let terminalInputs = ['','',''];
+let terminalInputs = ['','','',''];
 let helpCommands = [
     ['ls', 'Display all files in a directory'],
     ['debug', 'Show all debug variables'],
     ['open', 'Open a file for viewing'],
     ['benjs','Built in js compiler for running programs on fs'],
-    ['clear', 'Clears terminal']
+    ['clear', 'Clears terminal'],
+    ['load', 'Loads and run program from the internet']
 ];
 let tempFilesInternet = [];
+const allowedConsoles = [2, 3, 4];
 
 function runCode(num, displayType) {
     let code = document.getElementById('codeInput' + String(num)).value;
     code = parseCode(num, displayType, code);
-    runCodeFromString(code);
+    runCodeFromString(code, num, displayType);
     finaliseDisplaying(num, displayType);
 }
 
@@ -32,17 +34,15 @@ function parseCode(num, displayType, code) {
     vars = getVars(vars, 'let', code);
     vars = getVars(vars, 'var', code);
     vars = getVars(vars, 'const', code);
-    console.log(vars);
     let debug = toString(vars, num);
     let debugCode;
     debugCode = code + debug;
     return debugCode;
 }
 
-function runCodeFromString(code) {
+function runCodeFromString(code, num, displayType = 1) {
     try {
         const wrappedCode = `(function() { ${code} })()`;
-        console.log('wrapped cd:', wrappedCode);
         const result = eval(wrappedCode);
 
         if (JSON.stringify(result) !== undefined) {
@@ -51,7 +51,12 @@ function runCodeFromString(code) {
             variables[resultVar[0]-1] = resultVar[1];
         }
     } catch (error) {
-        window.alert('Error: ' + error.message);
+        if (displayType === 2) {
+            displayOnTerm(num, 'Error: ' + error.message);
+        } else {
+            window.alert('Error: ' + error.message);
+        }
+        return false;
     }
 }
 
@@ -114,7 +119,6 @@ function debugCode(num) {
     debugList.style.display = 'block';
     let tempHTML = '';
     let vars = variables[num-1];
-    console.log(vars);
     for (let i = 0; i < vars.length; i++) {
         let cVar = vars[i];
         tempHTML += '<b>' + cVar.name + '</b>: ' + cVar.val + '<br>';
@@ -168,8 +172,9 @@ function waitForInput(num, stringInput) {
     displayTerminal.innerHTML += '> ' + String(stringInput) + '<br>';
 }
 
-let flickerKey2 = setInterval(() => flickerKeyForTerminal(2), 500);
-let flickerKey3 = setInterval(() => flickerKeyForTerminal(3), 500);
+setInterval(() => flickerKeyForTerminal(2), 500);
+setInterval(() => flickerKeyForTerminal(3), 500);
+setInterval(() => flickerKeyForTerminal(4), 500);
 
 function flickerKeyForTerminal(num) {
     const displayTerminal = document.getElementById('console' + String(num));
@@ -205,6 +210,9 @@ function executeCode(num) {
     switch (executeString[0]) {
         case 'ls' : 
             fetchFiles().then(files => {
+                for (let i = 0; i < tempFilesInternet.length; i++) {
+                    files.push(tempFilesInternet[i]);
+                }
                 for (let i = 0; i < files.length; i++) {
                     displayOnTerm(num, ' - ' + files[i].name);
                 }
@@ -216,31 +224,39 @@ function executeCode(num) {
             break;
         case 'open':
             fetchFiles().then(files => {
-                console.log(files);
                 const foundFile = files.find(file => file.name === executeString[1]);
                 if (foundFile !== undefined) {
                     displayOnTerm(num, foundFile.content.replace(/\n/g, '<br>&ensp;'));
                 } else {
-                    displayOnTerm(num, 'File not found');
+                    const localFileFound = files.find(file => file.name === executeString[1]);
+                    if (localFileFound !== undefined) {
+                        displayOnTerm(num, localFileFound.content.replace(/\n/g, '<br>&ensp;'));
+                    } else {
+                        displayOnTerm(num, 'File not found ' + error);
+                    }
                 }
                 finaliseDisplaying(num, 2);
             }).catch(error => {
-                displayOnTerm(num, 'File not found' + error);
+                displayOnTerm(num, 'File not found: ' + error);
                 finaliseDisplaying(num, 2);
             });
             break;
         case 'benjs':
             fetchFiles().then(files => {
-                console.log(files);
                 const foundFile = files.find(file => file.name === executeString[1]);
                 if (foundFile !== undefined) {
-                    runCodeFromString(parseCode(num, 2, foundFile.content));
+                    runCodeFromString(parseCode(num, 2, foundFile.content), num, 2);
                 } else {
-                    displayOnTerm(num, 'File not found');
+                    const localFileFound = files.find(file => file.name === executeString[1]);
+                    if (localFileFound !== undefined) {
+                        runCodeFromString(parseCode(num, 2, localFileFound.content), num, 2);
+                    } else {
+                        displayOnTerm(num, 'File not found ' + error);
+                    }
                 }
                 finaliseDisplaying(num, 2);
             }).catch(error => {
-                displayOnTerm(num, 'File not found' + error);
+                displayOnTerm(num, 'File not found');
                 finaliseDisplaying(num, 2);
             });
             break;
@@ -260,8 +276,22 @@ function executeCode(num) {
         case 'load':
             loadFrom(executeString[1]).then(code => {
                 let filePath = executeString[1];
-                let name = filePath.split('/')
-                runCodeFromString(parseCode(num, 2, code));
+                let pathSplit = filePath.split('/');
+                let name;
+
+                if (pathSplit.length > 0) {
+                    name = pathSplit.slice(-1);
+                } else {
+                    name = 'unknown';
+                }
+                
+                let success = runCodeFromString(parseCode(num, 2, code), num, 2);
+                if (success !== false) {
+                    tempFilesInternet.push({
+                        'name': name,
+                        'content': code
+                    });
+                }
                 finaliseDisplaying(num, 2);
             }).catch(error => {
                 displayOnTerm(num, 'Error: ' + error.message);
@@ -316,15 +346,13 @@ function pasteFromClipboard(event) {
     
     if (clipboardData) {
         const text = clipboardData.getData('text');
-        if (focusOnTerm >= 2 && focusOnTerm <= 3) {
-            if (terminalInputs[focusOnTerm-1].slice(-1) === 'v') {
-                deleteCharOfTerminal(focusOnTerm);
-            }
-            for (let i = 0; i < text.length; i++) { 
-                terminalInputs[focusOnTerm-1] += text[i];
-                insertKeysToTerminal(focusOnTerm);
-            } 
+        if (terminalInputs[focusOnTerm-1].slice(-1) === 'v') {
+            deleteCharOfTerminal(focusOnTerm);
         }
+        for (let i = 0; i < text.length; i++) { 
+            terminalInputs[focusOnTerm-1] += text[i];
+            insertKeysToTerminal(focusOnTerm);
+        } 
     } else {
         console.error('Clipboard data is not available.');
     }
@@ -340,6 +368,10 @@ document.addEventListener('click', function(event) {
             const displayTerminal = document.getElementById('console3');
             displayTerminal.focus();    
             focusOnTerm = 3;
+        } else if (event.target.id === 'console4') {
+            const displayTerminal = document.getElementById('console4');
+            displayTerminal.focus();    
+            focusOnTerm = 4;
         } else {
             focusOnTerm = 0;
         }
@@ -351,7 +383,7 @@ document.addEventListener('click', function(event) {
 document.addEventListener('keydown', function(event) {
     let key = event.key;
     if (state) {
-        if ([2, 3].includes(focusOnTerm)) {
+        if (allowedConsoles.includes(focusOnTerm)) {
             if (/^[a-zA-Z]$/.test(key) || ['.',':','/'].includes(key)) {
                 terminalInputs[focusOnTerm-1] += key
                 insertKeysToTerminal(focusOnTerm);
@@ -365,7 +397,7 @@ document.addEventListener('keydown', function(event) {
         }
         if (focusOnTerm === 3) {
             if (key === 'Enter') {
-                executeCode(3);
+                executeCode(focusOnTerm);
             }
         }
     }
@@ -374,3 +406,39 @@ document.addEventListener('keydown', function(event) {
 document.addEventListener('paste', function(event) {
     pasteFromClipboard(event); 
 });
+
+/*
+document.addEventListener('DOMContentLoaded', () => {
+    const fileName = 'test.js';
+    const num = 4;
+    
+    displayOnTerm(num, '');
+    fetchFiles().then(files => {
+        const foundFile = files.find(file => file.name === fileName);
+        if (foundFile !== undefined) {
+            runCodeFromString(parseCode(num, 2, foundFile.content), num, 2);
+        } else {
+            displayOnTerm(num, 'Error: ' + error.message);
+        }
+        finaliseDisplaying(num, 2);
+    }).catch(error => {
+        displayOnTerm(num, 'Error: ' + error.message);
+        finaliseDisplaying(num, 2);
+    });
+});
+*/
+
+/*
+document.addEventListener('DOMContentLoaded', () => {
+    const internetPath = 'http://localhost:5173/test2.js';
+    const num = 4;
+    displayOnTerm(num, '');
+    loadFrom(internetPath).then(code => {
+        runCodeFromString(parseCode(num, 2, code), num, 2);
+        finaliseDisplaying(num, 2);
+    }).catch(error => {
+        displayOnTerm(num, 'Error: ' + error.message);
+        finaliseDisplaying(num, 2);
+    });
+});
+*/
--- test2.js
+++ test2.js
@@ -1,5 +1,5 @@
 let i = 0;
 console.log(i + 2);
 let p = 5;
-let q = 7;
+let q = prompt('Enter a number');
 console.log(p+q);
