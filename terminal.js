5f2f1c7d88387a3eb3c2352dc89b17bf0420a850
Ben <ben.varea@gmail.com>
2024-11-10 15:11:27 +0000

    Fully working commit - just missing prompt functionality

--- terminal.js
+++ terminal.js
@@ -1,3 +1,4 @@
+const terminalName = 'liveconsole';
 const helpCommands = [
     ['ls', 'Display all files in a directory'],
     ['debug', 'Show all debug variables'],
@@ -9,13 +10,17 @@ const helpCommands = [
 let focusTerminal = 0;
 
 class Terminal {
-    constructor(id, displayType, title = '') {
+    constructor(id, displayType, title = '', user = 'localhost', fsPath = '/files.json') {
         this.id = id;
         this.displayType = displayType;
+        this.user = user;
+        this.fsPath = fsPath;
+
         this.terminalInput = '';
         this.vars = [];
         this.tempFilesInternet = [];
         this.state = true; // If it's free to process other things
+        this.inputBlock = false;
 
         //this.initListeners = this.initListeners.bind(this);
 
@@ -48,7 +53,7 @@ class Terminal {
                         <div class="console-btn-col">
                             <button class='terminal-btn' id="runCode${this.id}">►</button>
                         </div>
-                        <div class="console" id="terminal${this.id}">[yourname]@liveconsole ~  <br></div>
+                        <div class="console" id="terminal${this.id}">[${this.user}]@${terminalName} ~  <br></div>
                     </div>
                     <br>
                 `;
@@ -58,7 +63,7 @@ class Terminal {
                 terminalDiv.innerHTML += `
                     <h1>${title}</h1>
                     <div class="code-box">
-                        <div class="console" id="terminal${this.id}">[yourname]@liveconsole ~  <br></div>
+                        <div class="console" id="terminal${this.id}">[${this.user}]@${terminalName} ~  <br></div>
                     </div>
                 `;
                 break;
@@ -67,53 +72,198 @@ class Terminal {
 
     initListeners() {
         document.addEventListener('click', (event) => {
-            console.log(event.target.id)
             if (event.target.matches('div')) { 
                 if (event.target.id === `terminal${this.id}`) {
                     focusTerminal = this.id;
                 }
             } else if ([1,2].includes(this.id)) {
                 if (event.target.id === `debugCode${this.id}`) {
-                    console.log('2a');
                     this.debugCode();
                 } else if (event.target.id === `runCode${this.id}`) {
-                    console.log('asda')
                     this.runCode();
                 }
             }
 
         });
-        document.addEventListener('keydown', (event) => {
-            let key = event.key;
-            if (this.state === true && this.id === focusTerminal) {
-                if (/^[a-zA-Z]$/.test(key) || ['.', ':', '/'].includes(key)) {
-                    this.insertKeyToTerminal(key);
-                } else if (key === 'Backspace') {
-                    this.deleteKeyOfTerminal();
-                } else if (key === ' ') {
-                    event.preventDefault();
-                    this.insertKeyToTerminal(key);
-                } else if (this.displayType === 3) {
-                    if (key === 'Enter') {
-                        //this.executeCode(focusTerminal);
-                    }
-                }
-            }
-        });
-        document.addEventListener('paste', (event) => {
-            console.log('yes', focusTerminal, this.id);
-            if (focusTerminal === this.id) {
-                this.pasteFromClipboard(event); 
-            }
-        });
         if ([2,3,4].includes(this.id)) {
             setInterval(() => flickerKeyTerminal(this.id, this.state), 500);
+            document.addEventListener('keydown', (event) => {
+                let key = event.key;
+                if (this.state === true && this.id === focusTerminal) {
+                    if (/^[a-zA-Z]$/.test(key) || ['.', ':', '/'].includes(key)) {
+                        this.insertKeyToTerminal(key);
+                    } else if (key === 'Backspace') {
+                        this.deleteKeyOfTerminal();
+                    } else if (key === ' ') {
+                        event.preventDefault();
+                        this.insertKeyToTerminal(key);
+                    } else if (this.displayType === 3) {
+                        if (key === 'Enter') {
+                            this.executeCode();
+                        }
+                    } else if (this.displayType === 4) {
+                        if (key === 'Enter') {
+                            this.executeCode(true);
+                        }
+                    }
+                }
+            });
+            document.addEventListener('paste', (event) => {
+                if (focusTerminal === this.id) {
+                    this.pasteFromClipboard(event); 
+                }
+            });
+            document.addEventListener('messageBroadcast', (event) => {
+                const message = event.detail;
+                if (message.id === this.id) {
+                    this.displayOnTerm(message.string);
+                } 
+            });
+            document.addEventListener('inputBroadcast', (event) => {
+                const message = event.detail;
+                if (message.id === this.id) {
+                    this.waitForInput(message.string);
+                } 
+            });
+        }
+    }
+
+    initOnload(path, type = true) { // Type is (true for link) and (false for file inside fs)
+        if (this.id === 4) {
+            if (type === true) {
+                this.displayOnTerm('');
+                loadFrom(path).then(code => {
+                    this.runCodeFromString(parseCode(code, this.id, this.displayType));
+                    this.finaliseDisplaying();
+                }).catch(error => {
+                    this.displayOnTerm('Error: ' + error.message);
+                    this.finaliseDisplaying();
+                });
+            } else {
+                this.displayOnTerm('');
+                fetchFiles(this.fsPath).then(files => {
+                    const foundFile = files.find(file => file.name === path);
+                    if (foundFile !== undefined) {
+                        this.runCodeFromString(parseCode(foundFile.content, this.id, this.displayType));
+                    } else {
+                        this.displayOnTerm('Error: File not found');
+                    }
+                    this.finaliseDisplaying();
+                }).catch(error => {
+                    this.displayOnTerm('Error: ' + error.message);
+                    this.finaliseDisplaying();
+                });
+            }
+        }
+    }
+
+    executeCode(runBlock = false) {
+        const executeString = this.terminalInput.split(' ');
+        if (this.inputBlock !== true && runBlock === false) {
+            switch (executeString[0]) {
+                case 'ls' : 
+                    fetchFiles(this.fsPath).then(files => {
+                        for (let i = 0; i < this.tempFilesInternet.length; i++) {
+                            files.push(this.tempFilesInternet);
+                        }
+                        for (let i = 0; i < files.length; i++) {
+                            this.displayOnTerm(' - ' + files[i].name);
+                        }
+                        this.finaliseDisplaying();
+                    }).catch(error => {
+                        this.displayOnTerm('This directory is empty...');
+                        this.finaliseDisplaying();
+                    });
+                    break;
+                case 'open':
+                    fetchFiles(this.fsPath).then(files => {
+                        const foundFile = files.find(file => file.name === executeString[1]);
+                        if (foundFile !== undefined) {
+                            this.displayOnTerm(foundFile.content.replace(/\n/g, '<br>&ensp;'));
+                        } else {
+                            const localFileFound = files.find(file => file.name === executeString[1]);
+                            if (localFileFound !== undefined) {
+                                this.displayOnTerm(localFileFound.content.replace(/\n/g, '<br>&ensp;'));
+                            } else {
+                                this.displayOnTerm('File not found ' + error);
+                            }
+                        }
+                        this.finaliseDisplaying();
+                    }).catch(error => {
+                        this.displayOnTerm('File not found: ' + error);
+                        this.finaliseDisplaying();
+                    });
+                    break;
+                case 'benjs':
+                    fetchFiles(this.fsPath).then(files => {
+                        const foundFile = files.find(file => file.name === executeString[1]);
+                        if (foundFile !== undefined) {
+                            this.runCodeFromString(parseCode(foundFile.content, this.id, this.displayType));
+                        } else {
+                            const localFileFound = this.tempFilesInternet.find(file => file.name === executeString[1]);
+                            if (localFileFound !== undefined) {
+                                this.runCodeFromString(parseCode(localFileFound.content, this.id, this.displayType));
+                            } else {
+                                this.displayOnTerm('File not found ' + error);
+                            }
+                        }
+                        this.finaliseDisplaying();
+                    }).catch(error => {
+                        this.displayOnTerm('File not found: ' + error);
+                        this.finaliseDisplaying();
+                    });
+                    break;
+                case 'help':
+                    for (let i = 0; i < helpCommands.length; i++) {
+                        this.displayOnTerm(helpCommands[i][0] + ' --- ' + helpCommands[i][1]);
+                    }
+                    this.finaliseDisplaying();
+                    break;
+                case 'clear':
+                    this.clearTerminal();
+                    break;
+                case 'debug':
+                    this.displayDebugOnTerm();
+                    this.finaliseDisplaying();
+                    break;
+                case 'load':
+                    loadFrom(executeString[1]).then(code => {
+                        let filePath = executeString[1];
+                        let pathSplit = filePath.split('/');
+                        let name;
+        
+                        if (pathSplit.length > 0) {
+                            name = pathSplit.slice(-1);
+                        } else {
+                            name = 'unknown';
+                        }
+                        
+                        let success = this.runCodeFromString(parseCode(code, this.id, this.displayType));
+                        if (success !== false) {
+                            tempFilesInternet.push({
+                                'name': name,
+                                'content': code
+                            });
+                        }
+                        this.finaliseDisplaying();
+                    }).catch(error => {
+                        this.displayOnTerm('Error: ' + error.message);
+                        this.finaliseDisplaying();
+                    });
+                    break;
+                default:
+                    this.displayOnTerm('Command <u>' + executeString[0] + '</u> not found');
+                    this.finaliseDisplaying();
+                    break;
+            }
+        } else {
+            this.unblockInput(executeString);
         }
     }
 
     runCode() {
         let code = document.getElementById(`codeInput${this.id}`).value;
-        code = parseCode(this.id, this.displayType, code);
+        code = parseCode(code, this.id, this.displayType);
         this.runCodeFromString(code);
         this.finaliseDisplaying();
     }
@@ -124,13 +274,13 @@ class Terminal {
             const result = eval(wrappedCode);
     
             if (JSON.stringify(result) !== undefined) {
-                resultVar = JSON.stringify(result);
+                let resultVar = JSON.stringify(result);
                 resultVar = JSON.parse(resultVar);
                 this.vars = resultVar[1];
             }
         } catch (error) {
             if ([2,3,4].includes(this.displayType)) {
-                this.displayOnTerm(num, 'Error: ' + error.message);
+                this.displayOnTerm('Error: ' + error.message);
             } else {
                 window.alert('Error: ' + error.message);
             }
@@ -138,11 +288,50 @@ class Terminal {
         }
     }
 
-    displayOnTerm(stringInput) {
-        const displayTerminal = document.getElementById(`terminal${this}`);
+    displayOnTerm(stringInput, override = false) {
+        const displayTerminal = document.getElementById(`terminal${this.id}`);
+        let displayWait = setInterval(() => {
+            if (this.inputBlock === false || override === true) {
+                this.state = false;
+                displayTerminal.innerHTML = displayTerminal.innerHTML.slice(0, -1) + '<br>&ensp;';
+                displayTerminal.innerHTML += String(stringInput) + ' ';
+                this.state = true;
+                clearInterval(displayWait);
+            }
+        }, 100);
+    }
+
+    displayDebugOnTerm() {
+        let tempHTML = '';
+        let vars = this.vars;
+        for (let i = 0; i < vars.length; i++) {
+            let cVar = vars[i];
+            tempHTML += '&ensp;<b>' + cVar.name + '</b>: ' + cVar.val;
+            if (i !== vars.length - 1) {
+                tempHTML += '<br>'
+            }
+        }
+        this.displayOnTerm(tempHTML);
+    }
+
+    clearTerminal() {
+        const displayTerminal = document.getElementById(`terminal${this.id}`);
         this.state = false;
-        displayTerminal.innerHTML = displayTerminal.innerHTML.slice(0, -1) + '<br>&ensp;';
-        displayTerminal.innerHTML += String(stringInput) + ' ';
+        displayTerminal.innerHTML = `[${this.user}]@${terminalName} ~  `;
+        this.terminalInput = [];
+        this.state = true;
+    }
+    
+    waitForInput(stringInput) {
+        const displayTerminal = document.getElementById(`terminal${this.id}`);
+        this.inputBlock = true;
+        this.state = true;
+        this.displayOnTerm(stringInput + ' ', true);
+    }
+
+    unblockInput(value) {
+        console.log(value);
+        this.inputBlock = false;
     }
 
     debugCode() {
@@ -177,13 +366,19 @@ class Terminal {
     }
 
     finaliseDisplaying() {
-        if (this.displayType === 2) {
-            const displayTerminal = document.getElementById(`terminal${this.id}`);
-            displayTerminal.innerHTML = displayTerminal.innerHTML.slice(0, -1) + ' ';
-            displayTerminal.innerHTML += '<br>[yourname]@liveconsole ~  ';
+        if ([2,3,4].includes(this.displayType)) {
+            let displayWait = setInterval(() => {
+                if (this.inputBlock === false) {
+                    this.state = false;
+                    const displayTerminal = document.getElementById(`terminal${this.id}`);
+                    displayTerminal.innerHTML = displayTerminal.innerHTML.slice(0, -1) + ' ';
+                    displayTerminal.innerHTML += `<br>[${this.user}]@${terminalName} ~  `;
+                    this.state = true;
+                    this.terminalInput = '';
+                    clearInterval(displayWait);
+                }
+            }, 100);
         }
-        this.state = true;
-        this.terminalInput = '';
     }
 
     pasteFromClipboard(event) {
@@ -202,16 +397,30 @@ class Terminal {
     }
 }
 
+function globalDisplayOnTerm(id, stringInput) {
+    const broadcastMessage = new CustomEvent('messageBroadcast', {
+        detail: {id: id, string: stringInput},
+      });
+      document.dispatchEvent(broadcastMessage);
+}
+
+function globalWaitForInput(id, stringInput) {
+    const broadcastMessage = new CustomEvent('inputBroadcast', {
+        detail: {id: id, string: stringInput},
+      });
+      document.dispatchEvent(broadcastMessage);
+}
+
 function removeLastChar(str) {
     return str.length > 0 ? str.slice(0, -1) : str;
 }
 
-function parseCode(id, displayType, code) {
+function parseCode(code, id, displayType) {
     code = code.replace(/\/\/# sourceMappingURL=.*$/, '').trim();
 
-    if (displayType === 2) {
-        code = code.replaceAll('console.log(', 'displayOnTerm(' + String(num) + ',');
-        code = code.replaceAll('prompt(', 'waitForInput(' + String(num) + ',');
+    if ([2,3,4].includes(displayType)) {
+        code = code.replaceAll('console.log(', 'globalDisplayOnTerm(' + String(id) + ',');
+        code = code.replaceAll('prompt(', 'globalWaitForInput(' + String(id) + ',');
     } else {
         code = code.replaceAll('console.log', 'window.alert');
     }
@@ -227,7 +436,7 @@ function parseCode(id, displayType, code) {
 }
 
 function varsToDebug(vars, id) {
-    let stringAcc = ';return [' + String(id) + ', [';
+    let stringAcc = '\nreturn [' + String(id) + ', [';
     for (let i = 0; i < vars.length; i++) {
         let varName = vars[i].name;
         stringAcc += `{'name': '${varName}', 'val': ${varName}}`;
@@ -310,8 +519,8 @@ function loadFrom(internetPath) {
     });
 }
 
-function fetchFiles() {
-    return fetch('files.json')
+function fetchFiles(fsPath) {
+    return fetch(fsPath)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
